13256123a8008b143bdb595f7bb8d7005ef7953c
priya-kinthali <priya-kinthali@github.com>
2025-03-12 14:39:39 +0530

    repro

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,6 @@
+import messenger from 'webext-messenger';
+const unusedVar = 42; // This should trigger a warning/error
+console.log('Hello, world!'); // This should be fine
 const { getJestProjects } = require('@nrwl/jest');
 
 export default { projects: getJestProjects() };
